ff0bd0f63827f0bd54d15c3ce07a0c3362ef675c
retorta <retortakolba@yandex.ru>
2017-12-08 00:33:51 +0300

    STATICDIR

--- dataset/settings.py
+++ dataset/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u_s0$3!!kkzmb_l8sx6yg#@4k0&z$%0s&!p(mvv%hq*^i%b(t@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,5 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+)
