72a6b21bdd33a37b1d8a22d6ca5408100565e49d
retorta <retortakolba@yandex.ru>
2017-12-12 03:27:13 +0300

    parse data.mos.ru

--- data/models.py
+++ data/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class DataSource(models.Model):
-    id = models.AutoField(primary_key=True,unique=True)
+    id = models.AutoField(primary_key=True, unique=True)
     name = models.TextField()
     url = models.URLField(blank=True)
     def __str__(self):
@@ -12,17 +12,20 @@ class ScienceField(models.Model):
     id = models.AutoField(primary_key=True,unique=True)
     name = models.TextField()
     description = models.TextField(blank=True)
+    metaid = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return str(self.id) + ' ' + self.name
 
 class DataSet(models.Model):
     id = models.AutoField(primary_key=True,unique=True)
+    size = models.IntegerField(blank=True, null=True)
     lastupdate = models.DateTimeField(auto_now=True)
     origname = models.TextField()
     engname = models.TextField(blank=True)
     url = models.URLField(blank=True)
     datasource = models.ForeignKey(DataSource, models.CASCADE, related_name='set')
-    sciencefield = models.ForeignKey(DataSource, models.CASCADE, related_name='dataset')
+    sciencefield = models.ForeignKey(ScienceField, models.CASCADE, related_name='dataset')
+    description = models.TextField(blank=True)
     def __str__(self):
         return str(self.id) + ' ' + self.origname
 
--- data/views.py
+++ data/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from data.models import *
-
+from django.core.exceptions import ObjectDoesNotExist
 def datasets(request):
     datasets = DataSet.objects.all()
     return render(request, 'datasets.html', {'datasets':datasets})
@@ -8,7 +8,8 @@ def datasets(request):
 def dataset(request, id):
     try:
         dataset = DataSet.objects.get(id=id)
-        return render(request, 'dataset.html', {'dataset':dataset})
+        fields = sorted(Field.objects.filter(dataset=dataset), key=lambda x:x.num)
+        return render(request, 'dataset.html', {'dataset':dataset, 'fields': fields})
     except ObjectDoesNotExist:
         return HttpResponseNotFound("<h1>Такого датасета нет</h1>")
 
@@ -19,7 +20,9 @@ def datasources(request):
 def datasource(request, id):
     try:
         datasource = DataSource.objects.get(id=id)
-        return render(request, 'datasource.html', {'datasource':datasource})
+        ds = DataSet.objects.filter(datasource=datasource)
+        print(ds)
+        return render(request, 'datasource.html', {'datasource':datasource, 'datasets':ds})
     except ObjectDoesNotExist:
         return HttpResponseNotFound("<h1>Такого источника данных нет</h1>")
 
@@ -31,12 +34,46 @@ def sciencefields(request):
 def sciencefield(request, id):
     try:
         sciencefield = ScienceField.objects.get(id=id)
-        return render(request, 'sciencefield.html', {'sciencefield':sciencefield})
+        ds = DataSet.objects.filter(sciencefield=sciencefield)
+        return render(request, 'sciencefield.html', {'sciencefield':sciencefield, 'datasets':ds})
     except ObjectDoesNotExist:
         return HttpResponseNotFound("<h1>Такой области нет</h1>")
 
+
+
+
+
+from urllib.request import *
+import json
+from datetime import datetime
+api_key = "api_key=35343aed729d9080f3d2fcaf98812838"
+def getdataset(_id):
+    try:
+        a = urlopen("https://apidata.mos.ru/v1/datasets/" + str(_id) + "?" + api_key)
+        return json.loads(a.read())
+    except:
+        return None #json.loads('{}')
+
+def gorcscf(metaid, name=''):
+    d, c = ScienceField.objects.get_or_create(metaid=metaid)
+    #print(d)
+    d.name = name
+    d.save()
+    return d
+
 def test(request):
-    pass
+    datasource = DataSource.objects.get(id=3)
+    for i in range(3300, 3400):
+        d = getdataset(i)
+        if d is not None:
+            scf = gorcscf(d['CategoryId'],d['CategoryCaption'])
+            dt = datetime.strptime(d['VersionDate'], '%d.%m.%Y')
+            dase, fl = DataSet.objects.get_or_create(size=d['ItemsCount'],lastupdate=dt,origname=d['Caption'],url='https://data.mos.ru/opendata/'+str(d['Id']),datasource=datasource,sciencefield=scf,description=d['FullDescription'] if d['FullDescription'] else d['Description'])
+            dase.save()
+            for j, field in enumerate(d['Columns']):
+                f, fl = Field.objects.get_or_create(name=field['Caption'], num = j, dataset = dase)
+                f.save()
+        print(i)
 
 def index (request):
     return render(request, 'index.html',{})
